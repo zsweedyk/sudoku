4e039b95d67b290c3109dd4c8bb4fa07a2a2d3b4
Josh <jpetrack@hmc.edu>
2014-09-11 16:01:15 -0700

    Added button that highlights and prints a message when pressed.  Completed tasks 1 and 2.

--- sudoku/JPViewController.m
+++ sudoku/JPViewController.m
@@ -8,7 +8,10 @@
 
 #import "JPViewController.h"
 
-@interface JPViewController ()
+@interface JPViewController () {
+    UIButton* _button;
+    UIView* _gridView;
+}
 
 @end
 
@@ -18,12 +21,45 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Create grid frame.
+    CGRect frame = self.view.frame;
+    CGFloat x = CGRectGetWidth(frame)*.1;
+    CGFloat y = CGRectGetHeight(frame)*.1;
+    CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame))*.80;
+    
+    CGRect gridFrame = CGRectMake(x,y,size,size);
+    
+    // Create grid view.
+    _gridView = [[UIView alloc] initWithFrame:gridFrame];
+    _gridView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:_gridView];
+    
+    // Create button.
+    CGFloat buttonSize = size / 5.0;
+    CGRect buttonFrame = CGRectMake(0,0,buttonSize,buttonSize);
+    _button = [[UIButton alloc] initWithFrame:buttonFrame];
+    _button.backgroundColor = [UIColor orangeColor];
+    [_gridView addSubview:_button];
+    
+    // Create target for button.
+    [_button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Change what the button says.
+    [_button setShowsTouchWhenHighlighted:YES];
+    [_button setTag:1];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)buttonPressed:(id)sender
+{
+    NSLog(@"Button 1 was pressed.");
 }
 
 @end
