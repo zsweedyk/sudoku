d552e4926757a96b9ecc0ffa78117521e5e7f69b
Rachel <rachelwilson@wl-205-143.wpa.claremont.edu>
2014-09-17 23:10:17 -0700

    Made stylistic improvements.

--- sudoku/JPGridView.m
+++ sudoku/JPGridView.m
@@ -2,8 +2,8 @@
 //  JPGridView.m
 //  sudoku
 //
-//  Created by Josh Petrack on 9/12/14.
-//  Copyright (c) 2014 Josh Petrack. All rights reserved.
+//  Created by Rachel on 9/12/14.
+//  Copyright (c) 2014 Rachel. All rights reserved.
 //
 
 #import "JPGridView.h"
@@ -20,50 +20,57 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        
-        for (int i = 0; i < 9; ++i) {
-            
-        }
-        
-        
         CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame));
-        // Create button.
-        CGFloat separationDistance = 1.0;
-        CGFloat buttonSize = ((size - 14 * separationDistance) / 9.0);
+        CGFloat thinSeparationDistance = 1.0;
+        CGFloat thickSeparationDistance = 2.0;
         
-        CGFloat currentY = 2 * separationDistance;
+        // On the Sudoku grid, there are 6 thin lines in each direction
+        // separating rows and columns, and 4 thicker lines.
+        CGFloat buttonSize = ((size - (6 * thinSeparationDistance + 4 * thickSeparationDistance)) / 9.0);
+        
+        CGFloat currentY = thickSeparationDistance;
+        CGFloat currentX;
         
         _buttons = [[NSMutableArray alloc] initWithCapacity:9];
         
         
-        for (int i = 0; i < 9; ++i) {
-            CGFloat currentX = 2 * separationDistance;
+        for (int row = 0; row < 9; ++row) {
+            // We want a thick padding on the border of the board.
+            currentX = thickSeparationDistance;
             
             NSMutableArray* currentRow;
             currentRow = [[NSMutableArray alloc] initWithCapacity:9];
-            for (int j = 0; j < 9; ++j) {
+            
+            for (int col = 0; col < 9; ++col) {
+                // Make button.
                 CGRect buttonFrame = CGRectMake(currentX,currentY,buttonSize,buttonSize);
-                
                 UIButton* _button = [[UIButton alloc] initWithFrame:buttonFrame];
                 _button.backgroundColor = [UIColor orangeColor];
                 [self addSubview:_button];
-                [_button setTag:9*i + j];
+                [_button setTag:9*row + col];
                 [_button setShowsTouchWhenHighlighted:YES];
                 [_button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
                 
-                [currentRow insertObject:_button atIndex:j];
-                if (j % 3 == 2) {
-                    currentX += separationDistance;
+                [currentRow insertObject:_button atIndex:col];
+                // 3x3 boxes should be separated with a thick border.
+                if (col % 3 == 2) {
+                    currentX += buttonSize + thickSeparationDistance;
                 }
-                currentX += buttonSize + separationDistance;
+                // other columns separated with thin border.
+                else {
+                    currentX += buttonSize + thinSeparationDistance;
+                }
             }
             
             // Insert the new sub-array of buttons into the main array.
-            [_buttons insertObject:currentRow atIndex:i];
-            if (i % 3 == 2) {
-                currentY += separationDistance;
+            [_buttons insertObject:currentRow atIndex:row];
+            // Same logic as above for border width.
+            if (row % 3 == 2) {
+                currentY += buttonSize + thickSeparationDistance;
             }
-            currentY += buttonSize + separationDistance;
+            else {
+                currentY += buttonSize + thinSeparationDistance;
+            }
         }
     }
     return self;
@@ -81,12 +88,12 @@
 }
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
 
 @end
